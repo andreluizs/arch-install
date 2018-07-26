1961372c7b9c035779494ba9e04575d6e0d868ae
André Luiz dos Santos <andreluizs@live.com>
2018-07-26 14:44:08 -0300

    Arch Rescue

--- files/boot-test.sh
+++ files/boot-test.sh
@@ -2,21 +2,7 @@
 set -o errexit
 set -o pipefail
 HD="/dev/sda"
-BOOT_ISO="/mnt/mnt/esp"
-BOOT_CHROOT="/mnt/esp"
-
-function _spinner(){
-    local pid=$2
-    local i=1
-    local param=$1
-    local sp='/-\|'
-    echo -ne "$param "
-    while [ -d /proc/"${pid}" ]; do
-        printf "[%c]   " "${sp:i++%${#sp}:1}"
-        sleep 0.75
-        printf "\\b\\b\\b\\b\\b\\b"
-    done
-}
+BOOT_MOUNT="/mnt/esp"
 
 function _chroot() {
     arch-chroot /mnt /bin/bash -c "$1"
@@ -40,34 +26,40 @@ function criar_volume_fisico(){
     parted -s ${HD} set 2 lvm on
     pvcreate -f "${HD}2" 
     vgcreate vg1 "${HD}2" 
-    lvcreate -L 50G -n root vg1
+    lvcreate -K -L 50G -n root vg1
 }
 
 function formatar_volume(){
     mkfs.vfat -F32 "${HD}1" -n BOOT
-    mkfs -F -t ext4 -L ROOT /dev/mapper/vg1-root 
+    mkfs.ext4 -F -L ROOT /dev/mapper/vg1-root
 }
 
 function montar_volume(){
     mount /dev/mapper/vg1-root /mnt 
-    mkdir -p ${BOOT_ISO} 
-    mount "${HD}1" ${BOOT_ISO}
+    mkdir -p "/mnt${BOOT_MOUNT}"
+    mount "${HD}1" "/mnt${BOOT_MOUNT}"
+    mkdir -p "/mnt${BOOT_MOUNT}/EFI/arch"
+    mkdir -p /mnt/boot
+    mount --bind "/mnt${BOOT_MOUNT}/EFI/arch" /mnt/boot
     lsblk ${HD}
 }
 
 function instalar_sistema(){
-    pacstrap /mnt base base-devel
+    pacstrap /mnt base base-devel intel-ucode
     genfstab -U -L /mnt >> /mnt/etc/fstab 
     _chroot "sed -i '/multilib]/,+1  s/^#//' /etc/pacman.conf"
+    _chroot "echo root:root | chpasswd"
 }
 
 function instalar_systemd_boot(){
     echo "Instalando o bootloader."
-    local loader="timeout 2\\\ndefault arch"
-    local arch_entrie="title Arch Linux\\\nlinux /vmlinuz-linux\\\ninitrd /initramfs-linux.img\\\noptions root=/dev/mapper/vg1-root rw"
-    _chroot "bootctl --path=${BOOT_CHROOT} install"
-    _chroot "echo \"${loader}\" > ${BOOT_CHROOT}/loader/loader.conf"
-    _chroot "echo \"${arch_entrie}\" > ${BOOT_CHROOT}/loader/entries/arch.conf"
+    local loader="timeout 0\ndefault arch"
+    local arch_entrie="title Arch Linux\\nlinux /EFI/arch/vmlinuz-linux\\n\\ninitrd  /EFI/arch/intel-ucode.img\\ninitrd /EFI/arch/initramfs-linux.img\\noptions root=/dev/mapper/vg1-root rw"
+    local arch_rescue="title Arch Linux (Rescue)\\nlinux /EFI/arch/vmlinuz-linux\\n\\ninitrd  /EFI/arch/intel-ucode.img\\ninitrd /EFI/arch/initramfs-linux.img\\noptions root=/dev/mapper/vg1-root rw\\nsystemd.unit=rescue.target"
+    _chroot "bootctl --path=${BOOT_MOUNT} install"
+    _chroot "echo -e \"${loader}\" > ${BOOT_MOUNT}/loader/loader.conf"
+    _chroot "echo -e \"${arch_entrie}\" > ${BOOT_MOUNT}/loader/entries/arch.conf"
+    _chroot "echo -e \"${arch_rescue}\" > ${BOOT_MOUNT}/loader/entries/arch-rescue.conf"
     _chroot "mkinitcpio -p linux"
 }
 
@@ -80,5 +72,6 @@ instalar_sistema
 instalar_systemd_boot
 echo "Sistema Instalado com Sucesso!"
 tree /mnt/mnt
-cat /mnt/mnt/esp/loader/loader.conf
-cat /mnt/mnt/esp/loader/entries/arch.conf
+cat "/mnt${BOOT_MOUNT}/loader/loader.conf"
+cat "/mnt${BOOT_MOUNT}/loader/entries/arch.conf"
+cat "/mnt${BOOT_MOUNT}/loader/entries/arch-rescue.conf"
